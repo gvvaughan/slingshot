2704b8e7e5b6344dd32728d7806917114b3a187d
Gary V. Vaughan <gary@gnu.org>
2013-04-28 11:03:55 +0700

    Initial import.
    Signed-off-by: Gary V. Vaughan <gary@gnu.org>

---
+++ specs/specs.mk
@@ -0,0 +1,41 @@
+# Specl make rules.
+#
+# Copyright (c) 2013 Gary V. Vaughan
+# Written by Gary V. Vaughan, 2013
+#
+# This program is free software; you can redistribute it and/or modify it
+# under the terms of the GNU General Public License as published by
+# the Free Software Foundation; either version 3, or (at your option)
+# any later version.
+#
+# This program is distributed in the hope that it will be useful, but
+# WITHOUT ANY WARRANTY; without even the implied warranty of
+# MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the GNU
+# General Public License for more details.
+#
+# You should have received a copy of the GNU General Public License
+# along with this program.  If not, see <http://www.gnu.org/licenses/>.
+
+
+## ------ ##
+## Specs. ##
+## ------ ##
+
+SPECL  = src/specl
+
+specl_SPECS =						\
+	$(srcdir)/specs/package_spec.yaml		\
+	$(NOTHING_ELSE)
+
+check_local += specs-check-local
+specs-check-local: $(SPECL) $(specl_SPECS) $(specl_LUASPECS)
+	$(AM_V_at)$(LUA) $(SPECL) $(SPECL_OPTS) $(specl_SPECS)
+
+
+## ------------- ##
+## Distribution. ##
+## ------------- ##
+
+EXTRA_DIST +=						\
+	$(specl_SPECS)					\
+	$(NOTHING_ELSE)
---
+++ src/src.mk
@@ -0,0 +1,59 @@
+# Project make rules.
+#
+# Copyright (c) 2013 Gary V. Vaughan
+# Written by Gary V. Vaughan, 2013
+#
+# This program is free software; you can redistribute it and/or modify it
+# under the terms of the GNU General Public License as published by
+# the Free Software Foundation; either version 3, or (at your option)
+# any later version.
+#
+# This program is distributed in the hope that it will be useful, but
+# WITHOUT ANY WARRANTY; without even the implied warranty of
+# MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the GNU
+# General Public License for more details.
+#
+# You should have received a copy of the GNU General Public License
+# along with this program.  If not, see <http://www.gnu.org/licenses/>.
+
+
+## ------ ##
+## Build. ##
+## ------ ##
+
+bin_SCRIPTS += build-aux/mkrockspecs
+
+man_MANS += docs/mkrockspecs.1
+
+docs/mkrockspecs.1.in: build-aux/mkrockspecs Makefile
+	@test -d docs || mkdir docs
+## Exit gracefully if mkrockspecs.1.in is not writeable, such as during distcheck!
+	$(AM_V_GEN)if ( touch $@.w && rm -f $@.w; ) >/dev/null 2>&1; \
+	then						\
+	  builddir='$(builddir)'			\
+	  $(srcdir)/build-aux/missing --run		\
+	    $(HELP2MAN)					\
+	      '--output=$@'				\
+	      '--no-info'				\
+	      '--name=Specl'				\
+	      src/specl;				\
+	fi
+
+
+## ------------- ##
+## Distribution. ##
+## ------------- ##
+
+EXTRA_DIST +=						\
+	build-aux/mkrockspecs				\
+	docs/mkrockspecs.1.in				\
+	$(NOTHING_ELSE)
+
+
+## ------------ ##
+## Maintenance. ##
+## ------------ ##
+
+CLEANFILES +=						\
+	docs/mkrockspecs.1				\
+	$(NOTHING_ELSE)
