172e898fba18d62151a59a8127ba8e9befb05e7c
Gary V. Vaughan <gary@gnu.org>
2014-01-04 13:25:00 +1300

    maint: post-release administrivia.
    * NEWS: Add header line for next release.
    * .prev-version: Record previous version.
    * ./local.mk (old_NEWS_hash): Auto-update.
    
    Signed-off-by: Gary V. Vaughan <gary@gnu.org>

--- local.mk
+++ local.mk
@@ -16,7 +16,7 @@
 # You should have received a copy of the GNU General Public License
 # along with this program.  If not, see <http://www.gnu.org/licenses/>.
 
-old_NEWS_hash = b7f5ee3470e07fef2c1a882767f9b99b
+old_NEWS_hash = 4a4a721668a1516c046d865c7b425586
 
 update_copyright_env = \
 	UPDATE_COPYRIGHT_HOLDER='(Free Software Foundation, Inc.|Gary V. Vaughan|Reuben Thomas)' \
